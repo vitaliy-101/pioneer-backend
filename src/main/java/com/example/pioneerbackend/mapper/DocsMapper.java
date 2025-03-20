ce62d7ec2ff8bb18d8d5c402149404ca967fb61b
Vitaliy <70775970+vitaliy-101@users.noreply.github.com>
2025-03-20 17:36:12 +0300

    docker-compose и jar файл для запуска

--- src/main/java/com/example/pioneerbackend/mapper/DocsMapper.java
+++ src/main/java/com/example/pioneerbackend/mapper/DocsMapper.java
@@ -3,7 +3,9 @@ package com.example.pioneerbackend.mapper;
 import com.example.pioneerbackend.dto.docs.*;
 import com.example.pioneerbackend.entity.docs.Docs;
 import com.example.pioneerbackend.entity.product.Product;
+
 import static com.example.pioneerbackend.util.FileUtils.convertFileToUrl;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
@@ -12,12 +14,13 @@ import org.mapstruct.Named;
 import java.util.List;
 
 import static com.example.pioneerbackend.constant.Urls.DOCS_FILE_URL;
+
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface DocsMapper {
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "name",  source = "docImageInfo.name")
-    @Mapping(target = "data",  source = "docImageInfo.data")
+    @Mapping(target = "name", source = "docImageInfo.name")
+    @Mapping(target = "data", source = "docImageInfo.data")
     @Mapping(target = "product", source = "product")
     @Mapping(target = "type", source = "type")
     Docs toEntity(Product product, DocType type, DocImageInfo docImageInfo);
